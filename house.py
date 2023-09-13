554ca238b7ce8cf7ccfb176347c70edd38c5f065
Techroze <119438223+Techroze@users.noreply.github.com>
2023-09-13 10:16:37 -0400

    Harry potter

---
+++ house.py
@@ -0,0 +1,11 @@
+#A system tells you what house your in based on name
+name = input("What's your name? ")
+
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
+
